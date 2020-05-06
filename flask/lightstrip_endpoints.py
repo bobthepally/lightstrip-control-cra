72ed16882b1dfc041eb835d3488f329817ce1ef0
Sean D <seandow382@gmail.com>
2020-05-06 09:42:28 -0700

    data validation on the input to the backend now happens

--- flask/lightstrip_endpoints.py
+++ flask/lightstrip_endpoints.py
@@ -7,6 +7,7 @@ app = Flask("lightstrip")
 @app.route("/set", methods=['POST'])
 def post_color_set():
 
+    # Read in the data from the request
     try:
         data = request.get_json()
         red     = data['red']
@@ -17,7 +18,22 @@ def post_color_set():
         print(msg, file=sys.stderr)
         return Response(msg, status=400, mimetype="text/plain")
 
-# TODO: Make sure these are numbers, and not the empty string
+    # Data validation section
+    try:
+        current_key = 'red'
+        int_red = int(red)
+
+        current_key = 'green'
+        int_green = int(green)
+
+        current_key = 'blue'
+        int_blue = int(blue)
+    except ValueError as e:    
+        msg = f"Color argument \'{current_key}\' is not an int"
+        print(msg, file=sys.stderr)
+        return Response(msg, status=400, mimetype="text/plain")
+
+    # Make call to the lightstrip API
     set_color(red, green, blue)
 
     return "Successfully set the lightstrip colors"
