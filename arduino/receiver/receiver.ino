c53a91920d02a95bd2e53b88bfb9f3a91c219e6c
Sean D <bobthepalley@gmail.com>
2021-11-29 06:34:50 +0000

    refactored the arduino to receive event-driven serial data

--- arduino/receiver/receiver.ino
+++ arduino/receiver/receiver.ino
@@ -9,7 +9,7 @@
 #define PIN 6
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 240
+#define NUMPIXELS 173
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
@@ -17,6 +17,11 @@
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 int delayval = 500; // delay for half a second
+
+// Global variable for the current color
+char currentColorArray[] = {0,0,0};
+
+// void singleColor(uint32_t mainColor);
 
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
@@ -32,20 +37,25 @@ void setup() {
 }
 
 void loop() {
-  
-  if (Serial.available() > 0) {
-    delay(50);
-    
-    char colorArray[] = {0,0,0};
-    Serial.readBytes(colorArray, 3);
 
-    uint32_t mainColor = pixels.Color(colorArray[0],colorArray[1],colorArray[2]);
+    uint32_t mainColor = pixels.Color(currentColorArray[0], currentColorArray[1], currentColorArray[2]);
     singleColor(mainColor);
-  }
 }
 
 void singleColor(uint32_t mainColor) {
   for (int i = 0; i < NUMPIXELS; i++)
     pixels.setPixelColor(i, mainColor);
   pixels.show();
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char tempArray[] = {0,0,0};
+    Serial.readBytes(tempArray, 3);
+
+    // Sets the main color
+    currentColorArray[0] = tempArray[0];
+    currentColorArray[1] = tempArray[1];
+    currentColorArray[2] = tempArray[2];
+  }
 }
