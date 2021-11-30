0969964a45e40a50b07d818704cbd0627579b00e
Sean D <seandow382@gmail.com>
2021-11-30 14:44:56 -0800

    implemented the hue color picker and added a clunky 'brightness' picker based on the AlphaSlider (don't judge me)

--- src/color-slider.js
+++ src/color-slider.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import Grid from '@material-ui/core/Grid';
 
-import { HuePicker } from 'react-color';
+import { HuePicker, AlphaPicker } from 'react-color';
 
 class ColorSlider extends Component {
     
@@ -11,19 +11,69 @@ class ColorSlider extends Component {
 
         this.state = {
             color: {
-                h: 0,
-                s: 0,
-                v: 0,
+                hsv: {
+                    h: 0,
+                    s: 1,
+                    v: 1,
+                    a: 1,
+                }
             }
         };
+
+        this.handleColorChange = this.handleColorChange.bind(this);
+        this.handleBrightnessChange = this.handleBrightnessChange.bind(this);
     }
+
+    handleColorChange(color) {
+
+        let hue = color.hsv.h;
+        let sat = color.hsv.s;
+        this.setState((prevState, props) => {
+            prevState.color.hsv.h = hue;
+            prevState.color.hsv.s = sat;
+            return prevState;
+        });
+
+        //console.log(this.state);
+    }
+
+    // we're going to convert the alpha value to the v of hsv
+    handleBrightnessChange(color) {
+        let brightness = color.hsv.a;
+
+        this.setState((prevState, props) => {
+            prevState.color.hsv.v = brightness;
+            return prevState;
+        });
+
+        //console.log(this.state);
+
+    }
+
 
     render() {
 
+        const colorValue = this.state.color;
+        //const test = {h: 100, s: 0, v: 0, a: 0.75}; 
+
+        const brightness = {
+            h: this.state.color.hsv.h,
+            s: this.state.color.hsv.s,
+            v: 1,
+            a: this.state.color.hsv.v
+        };
+
         return (
             <div className="colorslider">
-                <Grid>
-                    <HuePicker />
+                <Grid container spacing={1} alignItems="flex-start" direction="column">
+                    <Grid item>
+                        <HuePicker onChange={this.handleColorChange} color={colorValue} />
+                    </Grid>
+
+                    <Grid item>
+                        <AlphaPicker onChange={this.handleBrightnessChange} color={brightness} />
+                    </Grid>
+                
                 </Grid>
             </div>
         );
